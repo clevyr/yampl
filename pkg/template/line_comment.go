61db0f4a31511c0fe8ff95dd8cf03b526c535f0c
Gabe Cook <gabe565@gmail.com>
2022-05-06 09:55:36 -0500

    :recycle: Move if statement into template function to prevent undefined behavior

--- pkg/template/line_comment.go
+++ pkg/template/line_comment.go
@@ -10,10 +10,8 @@ import (
 
 func RecurseNode(conf config.Config, node *yaml.Node) error {
 	if len(node.Content) == 0 {
-		if node.LineComment != "" && strings.HasPrefix(node.LineComment, conf.Prefix) {
-			if err := TemplateLineComment(conf, node); err != nil {
-				return err
-			}
+		if err := TemplateLineComment(conf, node); err != nil {
+			return err
 		}
 	} else {
 		for _, node := range node.Content {
@@ -26,20 +24,22 @@ func RecurseNode(conf config.Config, node *yaml.Node) error {
 }
 
 func TemplateLineComment(conf config.Config, node *yaml.Node) error {
-	tmpl, err := template.New("").
-		Funcs(sprig.TxtFuncMap()).
-		Delims(conf.LeftDelim, conf.RightDelim).
-		Option("missingkey=error").
-		Parse(strings.TrimSpace(node.LineComment[len(conf.Prefix):]))
-	if err != nil {
-		return err
-	}
+	if node.LineComment != "" && strings.HasPrefix(node.LineComment, conf.Prefix) {
+		tmpl, err := template.New("").
+			Funcs(sprig.TxtFuncMap()).
+			Delims(conf.LeftDelim, conf.RightDelim).
+			Option("missingkey=error").
+			Parse(strings.TrimSpace(node.LineComment[len(conf.Prefix):]))
+		if err != nil {
+			return err
+		}
 
-	var buf strings.Builder
-	if err = tmpl.Execute(&buf, conf.Values); err != nil {
-		return err
-	}
+		var buf strings.Builder
+		if err = tmpl.Execute(&buf, conf.Values); err != nil {
+			return err
+		}
 
-	node.Value = buf.String()
+		node.Value = buf.String()
+	}
 	return nil
 }
