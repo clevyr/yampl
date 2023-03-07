4f32b675e9549e383f255b7f000dfebaf64e43ac
Gabe Cook <gabe565@gmail.com>
2023-03-06 21:51:35 -0600

    :recycle: (flags): Tweak newline fixer logic
    Newline-separated flags now get split into separate arguments instead of
    being joined with a comma

--- internal/util/flags.go
+++ internal/util/flags.go
@@ -1,32 +1,36 @@
 package util
 
 import (
+	"bufio"
 	"github.com/clevyr/yampl/cmd"
-	"regexp"
 	"strings"
 )
 
 func FixStringToStringNewlines(s []string) []string {
-	var replaceNext bool
+	var prevValueFlag string
+	result := make([]string, 0, len(s))
 	for i, arg := range s {
 		switch {
 		case arg == "--":
-			return s
-		case hasValueFlag(arg) || replaceNext:
-			replaceNext = false
-			if strings.ContainsRune(arg, '=') {
-				if strings.ContainsRune(arg, '\n') {
-					arg = regexp.MustCompile("\n+").ReplaceAllString(arg, ",")
-					arg = strings.Trim(arg, ",")
-					arg = strings.Replace(arg, "=,", "=", 1)
-					s[i] = arg
-				}
-			} else {
-				replaceNext = true
+			if prevValueFlag != "" {
+				result = append(result, prevValueFlag)
 			}
+			result = append(result, s[i:]...)
+			return result
+		case prevValueFlag != "":
+			result = append(result, fixArgNewlines(prevValueFlag+"="+arg)...)
+			prevValueFlag = ""
+		case hasValueFlag(arg):
+			if strings.ContainsRune(arg, '=') {
+				result = append(result, fixArgNewlines(arg)...)
+			} else {
+				prevValueFlag = arg
+			}
+		default:
+			result = append(result, arg)
 		}
 	}
-	return s
+	return result
 }
 
 func hasValueFlag(s string) bool {
@@ -34,4 +38,24 @@ func hasValueFlag(s string) bool {
 		s == "--"+cmd.ValueFlag ||
 		strings.HasPrefix(s, "-"+cmd.ValueFlagShort+"=") ||
 		strings.HasPrefix(s, "--"+cmd.ValueFlag+"=")
+}
+
+func fixArgNewlines(arg string) []string {
+	if strings.ContainsRune(arg, '\n') {
+		prefix, arg, found := strings.Cut(arg, "=")
+		if !found {
+			return []string{prefix}
+		}
+
+		result := make([]string, 0, 2)
+		s := bufio.NewScanner(strings.NewReader(arg))
+		for s.Scan() {
+			if len(s.Bytes()) > 0 {
+				result = append(result, prefix+"="+s.Text())
+			}
+		}
+		return result
+	} else {
+		return []string{arg}
+	}
 }
--- internal/util/flags_test.go
+++ internal/util/flags_test.go
@@ -16,12 +16,12 @@ func TestFixStringToStringNewlines(t *testing.T) {
 	}{
 		{"no value flag", args{[]string{"yampl"}}, []string{"yampl"}},
 		{"no newline", args{[]string{"yampl", "--value=a=a"}}, []string{"yampl", "--value=a=a"}},
-		{"newline with equal", args{[]string{"yampl", "--value=a=a\nb=b"}}, []string{"yampl", "--value=a=a,b=b"}},
-		{"newline with space", args{[]string{"yampl", "--value", "a=a\nb=b"}}, []string{"yampl", "--value", "a=a,b=b"}},
+		{"newline with equal", args{[]string{"yampl", "--value=a=a\nb=b"}}, []string{"yampl", "--value=a=a", "--value=b=b"}},
+		{"newline with space", args{[]string{"yampl", "--value", "a=a\nb=b"}}, []string{"yampl", "--value=a=a", "--value=b=b"}},
 		{"newline in file", args{[]string{"yampl", "test\nfile.yaml"}}, []string{"yampl", "test\nfile.yaml"}},
 		{"newline after end of options", args{[]string{"yampl", "-v=a=a", "---", "-v\nfile.yaml"}}, []string{"yampl", "-v=a=a", "---", "-v\nfile.yaml"}},
 		{"trim newline", args{[]string{"yampl", "-v=\na=a\n"}}, []string{"yampl", "-v=a=a"}},
-		{"collapse newlines", args{[]string{"yampl", "-v=a=a\n\nb=b"}}, []string{"yampl", "-v=a=a,b=b"}},
+		{"collapse newlines", args{[]string{"yampl", "-v=a=a\n\nb=b"}}, []string{"yampl", "-v=a=a", "-v=b=b"}},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
