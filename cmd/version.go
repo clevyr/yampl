76d99da8606f190e060b735af1ab15fe07ca187c
Gabe Cook <gabe565@gmail.com>
2023-10-06 00:19:32 -0500

    feat: Use short commit hash in version

--- cmd/version.go
+++ cmd/version.go
@@ -2,6 +2,9 @@ package cmd
 
 func buildVersion(version, commit string) string {
 	if commit != "" {
+		if len(commit) > 8 {
+			commit = commit[:8]
+		}
 		version += " (" + commit + ")"
 	}
 	return version
