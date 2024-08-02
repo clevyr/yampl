ec9b2998930192b6f88c8befde57f4d28f97e26c
Gabe Cook <gabe565@gmail.com>
2024-08-02 03:09:38 -0500

    chore(config): Group deprecated flags

--- internal/config/flags.go
+++ internal/config/flags.go
@@ -2,23 +2,22 @@ package config
 
 import (
 	"bytes"
+	"errors"
 
 	"github.com/rs/zerolog/log"
 	"github.com/spf13/cobra"
 )
 
 const (
+	VarFlag = "var"
+
 	InplaceFlag    = "inplace"
-	VarFlag        = "var"
-	ValueFlag      = "value"
-	RecursiveFlag  = "recursive"
 	PrefixFlag     = "prefix"
 	LeftDelimFlag  = "left-delim"
 	RightDelimFlag = "right-delim"
 	IndentFlag     = "indent"
 	StripFlag      = "strip"
 
-	FailFlag                 = "fail"
 	IgnoreUnsetErrorsFlag    = "ignore-unset-errors"
 	IgnoreTemplateErrorsFlag = "ignore-template-errors"
 
@@ -26,36 +25,41 @@ const (
 	LogFormatFlag = "log-format"
 
 	CompletionFlag = "completion"
+
+	ValueFlag = "value"
+	RecursiveFlag = "recursive"
+	FailFlag = "fail"
 )
 
 func (c *Config) RegisterFlags(cmd *cobra.Command) {
-	cmd.Flags().BoolVarP(&c.Inplace, InplaceFlag, "i", c.Inplace, "Edit files in place")
 	cmd.Flags().Var(c.valuesStringToString, ValueFlag, "Define a template variable. Can be used more than once.")
-	cmd.Flags().VarP(c.valuesStringToString, VarFlag, "v", "Define a template variable. Can be used more than once.")
-	if err := cmd.Flags().MarkDeprecated(ValueFlag, "use --"+VarFlag+" instead"); err != nil {
-		panic(err)
-	}
-	cmd.Flags().BoolP(RecursiveFlag, "r", true, "Recursively update yaml files in the given directory")
-	if err := cmd.Flags().MarkDeprecated(RecursiveFlag, cmd.Name()+" will always recurse if a given path is a directory"); err != nil {
-		panic(err)
-	}
+
+	cmd.Flags().BoolVarP(&c.Inplace, InplaceFlag, "i", c.Inplace, "Edit files in place")
 	cmd.Flags().StringVarP(&c.Prefix, PrefixFlag, "p", c.Prefix, "Template comments must begin with this prefix. The beginning '#' is implied.")
 	cmd.Flags().StringVar(&c.LeftDelim, LeftDelimFlag, c.LeftDelim, "Override template left delimiter")
 	cmd.Flags().StringVar(&c.RightDelim, RightDelimFlag, c.RightDelim, "Override template right delimiter")
 	cmd.Flags().IntVarP(&c.Indent, IndentFlag, "I", c.Indent, "Override output indentation")
 	cmd.Flags().BoolVarP(&c.Strip, StripFlag, "s", c.Strip, "Strip template comments from output")
 
-	cmd.Flags().BoolP(FailFlag, "f", false, `Exit with an error if a template variable is not set`)
 	cmd.Flags().BoolVar(&c.IgnoreUnsetErrors, IgnoreUnsetErrorsFlag, c.IgnoreUnsetErrors, "Exit with an error if a template variable is not set")
 	cmd.Flags().BoolVar(&c.IgnoreTemplateErrors, IgnoreTemplateErrorsFlag, c.IgnoreTemplateErrors, "Continue processing a file even if a template fails")
-	if err := cmd.Flags().MarkDeprecated(FailFlag, "use --"+IgnoreUnsetErrorsFlag+" and --"+IgnoreTemplateErrorsFlag+" instead"); err != nil {
-		panic(err)
-	}
 
 	cmd.Flags().StringVarP(&c.LogLevel, LogLevelFlag, "l", c.LogLevel, "Log level (trace, debug, info, warn, error, fatal, panic)")
 	cmd.Flags().StringVar(&c.LogFormat, LogFormatFlag, c.LogFormat, "Log format (auto, color, plain, json)")
 
 	cmd.Flags().StringVar(&c.Completion, CompletionFlag, c.Completion, "Output command-line completion code for the specified shell. Can be 'bash', 'zsh', 'fish', or 'powershell'.")
+
+	// Deprecated
+	cmd.Flags().VarP(c.valuesStringToString, VarFlag, "v", "Define a template variable. Can be used more than once.")
+	cmd.Flags().BoolP(RecursiveFlag, "r", true, "Recursively update yaml files in the given directory")
+	cmd.Flags().BoolP(FailFlag, "f", false, `Exit with an error if a template variable is not set`)
+	if err := errors.Join(
+		cmd.Flags().MarkDeprecated(ValueFlag, "use --"+VarFlag+" instead"),
+		cmd.Flags().MarkDeprecated(RecursiveFlag, cmd.Name()+" will always recurse if a given path is a directory"),
+		cmd.Flags().MarkDeprecated(FailFlag, "use --"+IgnoreUnsetErrorsFlag+" and --"+IgnoreTemplateErrorsFlag+" instead"),
+	); err != nil {
+		panic(err)
+	}
 
 	initLog(cmd)
 	cmd.Flags().SetOutput(DeprecatedWriter{})
